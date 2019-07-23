ab5e3ebabe69016bc69ca808d2b5ecd0b3a55972
Stanya Palmaro <spalmaro@student.42.fr>
2019-07-23 16:02:18 +0200

    setup of navbar start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss', './header.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'yr-poc';
+  product;
+  navbar;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+
+    // this.product = this.http.get('/api/search/product/alternateur-valeo-437433');
+    this.http.get('/api/search/product/alternateur-valeo-437433')
+      .subscribe(el => this.product = el );
+
+    this.http.get('http://yakarouler.local/header/top', {responseType: 'text'}).subscribe(res => {
+      this.navbar = res;
+      console.log(res);
+    });
+  }
 }
